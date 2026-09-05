35fca91d10a37b2a7f68c0265b7ccb2a4a8d92ee
Shubham Sharma <shubhamsharma8602230750@gmail.com>
2026-09-05 23:22:17 +0530

    feat: update auth screens

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -17,8 +17,7 @@ import { JetBrainsMono_500Medium } from "@expo-google-fonts/jetbrains-mono/500Me
 import { JetBrainsMono_600SemiBold } from "@expo-google-fonts/jetbrains-mono/600SemiBold";
 import { JetBrainsMono_700Bold } from "@expo-google-fonts/jetbrains-mono/700Bold";
 
-import HomeScreen from "./src/screens/buyer/HomeScreen";
-import SearchScreen from "./src/screens/buyer/SearchScreen";
+import AuthNavigator from "./src/navigation/AuthNavigator";
 
 export default function App() {
   const [interLoaded] = useFonts({
@@ -45,5 +44,5 @@ export default function App() {
     return null;
   }
 
-  return <SearchScreen />;
+  return <AuthNavigator />;
 }
